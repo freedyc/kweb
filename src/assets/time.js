ac7f2c82b6303ab9a3c4a284748b5fe6db8cc93c
dengyongchao <dengyongchao@zdns.cn>
2019-02-22 10:08:23 +0800

    Fix time

--- src/assets/time.js
+++ src/assets/time.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const Time = (props) => {
-    console.log(props);
-    return (
-        <div>时间：{props.time}</div>
-    )
+const refreshTime = () => {
+    return { type: 'REFRESH_TIME', time: new Date().toJSON() };
+}
+class Time extends React.Component {
+
+    componentWillMount() {
+        const { refreshTime } = this.props;
+        this.timeId = setInterval(() => refreshTime(), 1000);
+    }
+
+    componentWillUnmount() {
+        console.log('clear Interval')
+        clearInterval(this.timeId)
+    }
+
+    render() {
+        const { time } = this.props;
+        return (
+            <div>时间：{time}</div>
+        )
+    }
 };
 
 const mapStateToProps = (state) => ({
     time: state.time
 });
-export default connect(mapStateToProps)(Time);
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+    refreshTime,
+}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Time);
 
 
