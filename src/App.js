17023108cf0a985250db5d050d7bef47760f62a0
dengyongchao <dengyongchao@zdns.cn>
2020-01-17 09:01:52 +0800

    [cc]

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css'
-import Routes from './routes';
-import Menus from './common/components/menus';
-import './css/App.css';
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import Routes from "./routes";
+import Menus from "./common/components/menus";
+import "./css/App.css";
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <Menus />
-            <Routes />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Menus />
+                <Routes />
+            </div>
+        );
+    }
 }
 
 export default App;
